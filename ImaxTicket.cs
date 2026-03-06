2ac0bf1ad580a55d64f2521b76faa9a1dae4d25f
Emymohamamed <em0086651@gmail.com>
2026-03-07 01:52:26 +0200

    q

---
+++ ImaxTicket.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static G_NET_26_OOP_5.Program;
+
+namespace G_NET_26_OOP_5
+{
+    internal class ImaxTicket : Ticket, IPrintable, IBookable
+    {
+        public bool IsBooked { get; private set; }
+
+        public bool Book()
+        {
+            if (IsBooked)
+                return false;
+
+            IsBooked = true;
+            return true;
+        }
+
+        public bool Cancel()
+        {
+            if (!IsBooked)
+                return false;
+
+            IsBooked = false;
+            return true;
+        }
+
+        public void Print()
+        {
+            Console.WriteLine($"IMAX Ticket - Movie: {MovieName}, Price: {Price}");
+        }
+    }
+}
